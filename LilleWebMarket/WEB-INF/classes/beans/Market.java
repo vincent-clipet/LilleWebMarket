bfff6c9581931b16c80e32bd0ecdeb728ff71f94
mpiffault <martin.piffault@gmail.com>
2014-01-19 22:23:37 +0100

    ADD ::: Market bean

--- LilleWebMarket/WEB-INF/classes/beans/Market.java
+++ LilleWebMarket/WEB-INF/classes/beans/Market.java
@@ -4,40 +4,97 @@ package beans;
 public class Market
 {
 
-	//
-	// --- Attributes ----------------------------
-	//
+    //
+    // --- Attributes ----------------------------
+    //
+    private int market_id;
+    private String info;
+    private String opposite_info;
+    private String end_date; // TODO: Voir l'utilisation des types Date de java
+    private boolean result;
+    private int creator_id;
 	
+    //
+    // --- Constructors ----------------------------
+    //
 	
+    public Market()
+    {
 	
+    }
 	
+    public Market(int market_id, String info, String opposite_info, String end_date, boolean result, int creator_id)
+    {
+	this.market_id = market_id;
+	this.info = info;
+	this.opposite_info = opposite_info;
+	this.end_date = end_date;
+	this.result = result;
+	this.creator_id = creator_id;
+    }
 	
-	//
-	// --- Constructors ----------------------------
-	//
-	
-	public Market()
-	{
-		
-	}
-	
-	
-	
+    //
+    // --- Get & Set ----------------------------
+    //
+    
+    public void setMarket_id(int market_id)
+    {
+	this.market_id = market_id;
+    }
+
+    public int getMarket_id ()
+    {
+	return market_id;
+    }
 
 
-	//
-	// --- Methods ----------------------------
-	//
-	
-	
-	
-	
+    public void setInfo(String info)
+    {
+	this.info = info;
+    }
 
-	//
-	// --- Get & Set ----------------------------
-	//
+    public String getInfo ()
+    {
+	return info;
+    }
 
+    public void setOpposite_info(String opposite_info)
+    {
+	this.opposite_info = opposite_info;
+    }
 
+    public String getOpposite_info ()
+    {
+	return opposite_info;
+    }
 
+    public void setEnd_date(String end_date)
+    {
+	this.end_date = end_date;
+    }
 
+    public String getEnd_date ()
+    {
+	return end_date;
+    }
+
+    public void setResult(boolean result)
+    {
+	this.result = result;
+    }
+
+    public boolean getResult ()
+    {
+	return result;
+    }
+
+    public void setCreator_id(int creator_id)
+    {
+	this.creator_id = creator_id;
+    }
+
+    public int getCreator_id ()
+    {
+	return creator_id;
+    }
 }
