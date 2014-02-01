8569f3d08810f429c35f02df8d2f5eee36d45e71
mpiffault <martin.piffault@gmail.com>
2014-02-01 13:48:54 +0100

    ADD::: servlet.BuyServlet MODIFY::: market.jsp

---
+++ LilleWebMarket/WEB-INF/classes/servlet/BuyServlet.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+
+import dao.MarketDAO;
+import dao.DAOFactory;
+import beans.Market;
+import beans.Sell;
+import java.util.ArrayList;
+
+// import ;
+// import ;
+
+@WebServlet("/buy")
+    public class BuyServlet extends HttpServlet
+    {
+	
+	//
+	// ATTRIBUTES
+	//
+	private MarketDAO marketDao;
+
+	//
+	// METHODS
+	//
+	public void init() throws ServletException
+	{
+	    this.marketDao = ((DAOFactory) (getServletContext().getAttribute("dao_factory"))).getMarketDAO();
+	}
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+	    //	    HttpSession session = req.getSession(true);
+	    res.setContentType("text/html");
+	    PrintWriter out = res.getWriter();
+
+	    beans.Market m = (Market) (req.getAttribute("marketBean"));
+	    boolean opposite = false;
+	    int marketId = 0;
+
+	    try {opposite = Boolean.parseBoolean(req.getParameter("opposite"));}
+	    catch (Exception e) {}
+	    
+	    try {marketId = Integer.parseInt(req.getParameter("id"));}
+	    catch (Exception e) {}
+
+	    if (m == null)
+		{
+		    m = new Market();
+		}
+	    marketDao.getMarket(marketId, m);
+	
+	    ArrayList<Sell> asks = marketDao.getAsks(marketId, opposite);
+	    ArrayList<Sell> bids = marketDao.getBids(marketId, opposite);
+
+	    req.setAttribute("marketBean", m);
+	    req.setAttribute("marketId", marketId);
+	    req.setAttribute("opposite", opposite);
+	    req.setAttribute("asks", asks);
+	    req.setAttribute("bids", bids);
+
+	    RequestDispatcher dispatcher = req.getRequestDispatcher("market.jsp");
+	    dispatcher.forward(req, res);
+	}
+    }
--- LilleWebMarket/market.jsp
+++ LilleWebMarket/market.jsp
@@ -87,8 +87,10 @@
 	</div><!-- end div #bids -->
 
 	<div id='buy-form' >
-	  <form method="get" action="buy-servlet">
-	    <input type="text" name="marketid" value='<%= marketId %>' hidden/>
+	  <form method="get" action='buy'>
+	    <input type="text" name="id" value='<%= marketId %>' hidden/>
+	    <input type="text" name="opposite" value='<%= marketId %>' hidden/>
+
 	    <input type="text" name="quantity" placeholder="quantity"/>
 	    <input type="text" name="price" placeholder="price"/>
 	    <input type="submit" value="Faire une offre"/>
