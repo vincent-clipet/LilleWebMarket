86e7e095db5a0ec2d59a00ccb8f3a0047b3c2da6
vincent-clipet <vincent.clipet.7@gmail.com>
2014-02-02 20:06:24 +0100

    ADD ::: CustomHttpServlet, used to init vars & parameters common to every page EDIT ::: Profile servlet is now using CustomHttpServlet

---
+++ LilleWebMarket/WEB-INF/classes/servlet/CustomHttpServlet.java
@@ -0,0 +1,74 @@
+package servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import beans.User;
+import dao.DAOFactory;
+import dao.UserDAO;
+import dao.MarketDAO;
+import dao.StockDAO;
+
+public class CustomHttpServlet extends HttpServlet
+{ 
+
+	//
+	// ATTRIBUTES
+	//
+	protected HttpServletRequest req;
+	protected HttpServletResponse res;
+	protected PrintWriter out;
+	protected HttpSession session;
+	
+	protected UserDAO userDao;
+	protected MarketDAO marketDao;
+	protected StockDAO stockDao;
+	
+	
+
+	//
+	// METHODS
+	//	
+	protected void initInstance(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		this.req = req;
+		this.res = res;
+		this.out = res.getWriter();
+		this.session = req.getSession(true);
+		res.setContentType("text/html");
+		
+		DAOFactory factory = ((DAOFactory) (getServletContext().getAttribute("dao_factory")));
+		
+		if (userDao == null)
+			userDao = factory.getUserDAO();
+			
+		if (marketDao == null)
+			marketDao = factory.getMarketDAO();
+			
+		if (stockDao == null)
+			stockDao = factory.getStockDAO();
+	}
+	
+	protected void storeUser()
+	{
+		
+		User u = (User) ((req.getSession(true)).getAttribute("userBean"));
+		
+		if (u == null)
+		{
+			u = new User();
+			userDao.getUser(req.getUserPrincipal().getName(), u);
+			session.setAttribute("userBean", u);
+		}
+	}
+	
+	protected void sendToJsp(String jsp) throws ServletException, IOException
+	{
+		req.getRequestDispatcher(jsp).forward(req, res);
+	}
+	
+}
--- LilleWebMarket/WEB-INF/classes/servlet/Profile.java
+++ LilleWebMarket/WEB-INF/classes/servlet/Profile.java
@@ -1,54 +1,22 @@
 package servlet;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.servlet.RequestDispatcher;
-
-import beans.User;
-import dao.DAOFactory;
-import dao.UserDAO;
 
 @WebServlet("/profile")
-public class Profile extends HttpServlet
+public class Profile extends CustomHttpServlet
 {
-	
-	//
-	// ATTRIBUTES
-	//
-	private UserDAO userDao;
-	
-	
-	
+
 	//
 	// METHODS
 	//
-	public void init() throws ServletException
-	{
-		this.userDao = ((DAOFactory) (getServletContext().getAttribute("dao_factory"))).getUserDAO();
-	}
-	
 	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
 	{
-		res.setContentType("text/html");
-		PrintWriter out = res.getWriter();
-		HttpSession session = req.getSession(true);
+		super.initInstance(req, res);
+		super.storeUser();
 		
-		User u = (User) (session.getAttribute("userBean"));
-		
-		if (u == null)
-		{
-			u = new User();
-			userDao.getUser(req.getUserPrincipal().getName(), u);
-			session.setAttribute("userBean", u);
-		}
-		
-		RequestDispatcher dispatcher = req.getRequestDispatcher("profile.jsp");
-		dispatcher.forward(req, res);
+		super.sendToJsp("profile.jsp");
 	}
 }
